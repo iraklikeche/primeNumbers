f95134be3dc1229921a79160e204fd2958ad1892
iraklikeche <irakli.kechee@gmail.com>
2025-05-29 13:59:26 +0400

    feat:enhance-validation

--- app/Http/Requests/SubmitNumbersRequest.php
+++ app/Http/Requests/SubmitNumbersRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class SubmitNumbersRequest extends FormRequest
 {
@@ -12,7 +13,15 @@ class SubmitNumbersRequest extends FormRequest
     {
         return [
             'numbers' => ['required', 'array', 'max:10000'],
-            'numbers.*' => ['required', 'numeric'],
+            'numbers.*' => [
+                'required',
+                Rule::notIn(['']), 
+                function ($attribute, $value, $fail) {
+                    if (!is_int($value) && !is_float($value)) {
+                        $fail("The {$attribute} must be an integer or float, not a string.");
+                    }
+                },
+            ],
         ];
     }
 
